0b317e284d9fd767ee7bf738a798232761c66e93
Josh Ambala <JoshAmbala86@Jesleys-MBP.att.net>
2017-10-02 01:39:45 -0700

    fixing heroku bugs

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,3 @@
-
-// Here is the O.R.M. where you write functions that takes inputs and conditions
-// and turns them into database commands like SQL.
-
 var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
@@ -15,7 +11,7 @@ function printQuestionMarks(num) {
 }
 
 function objToSql(ob) {
-  // column1=value, column2=value2,...
+
   var arr = [];
 
   for (var key in ob) {
@@ -35,8 +31,7 @@ var orm = {
       cb(result);
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
+
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(methodOverride("_method"));
 
 var exphbs = require("express-handlebars");
