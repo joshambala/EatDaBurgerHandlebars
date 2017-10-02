fda59b78f47e3f1cff7a2f4484b171ad4a8885cd
Josh Ambala <JoshAmbala86@Jesleys-MBP.att.net>
2017-10-02 13:14:18 -0700

    app is working properly now.

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,7 @@
+
+// Here is the O.R.M. where you write functions that takes inputs and conditions
+// and turns them into database commands like SQL.
+
 var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
@@ -11,7 +15,7 @@ function printQuestionMarks(num) {
 }
 
 function objToSql(ob) {
-
+  // column1=value, column2=value2,...
   var arr = [];
 
   for (var key in ob) {
@@ -31,7 +35,8 @@ var orm = {
       cb(result);
     });
   },
-
+  // vals is an array of values that we want to save to cols
+  // cols are the columns we want to insert the values into
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
